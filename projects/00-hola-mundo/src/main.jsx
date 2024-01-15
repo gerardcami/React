69b9be422aa5535e0ac5021add1ed4ecd344d3aa
gcamig <gerard.camig@educem.net>
2024-01-15 10:29:09 +0100

    New component

---
+++ projects/00-hola-mundo/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.Fragment>
+    <button>Hola mundo</button>
+    <button>Hola mundo</button>
+    <button>Hola mundo</button>
+  </React.Fragment>,
+)
