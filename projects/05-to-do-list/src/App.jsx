665268694adb199e9f3ca0e37f05fab62b8a41d9
Gerard Camí <gerardcamigay@gmail.com>
2024-11-05 19:43:01 +0100

    Remove items

--- projects/05-to-do-list/src/App.jsx
+++ projects/05-to-do-list/src/App.jsx
@@ -31,29 +31,37 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(newTaskList));
   };
 
+  const handleRemoveTask = (item) => {
+    const newTaskList = taskList.filter(
+      (task) => item.objectID !== task.objectID
+    );
+    setTasks(newTaskList);
+    localStorage.setItem("tasks", JSON.stringify(newTaskList));
+  };
+
   return (
     <>
       <h1>To-do List</h1>
 
       <form className="flex flex-col gap-4" onSubmit={handleTaskInput}>
         <label htmlFor="newTask">New Task: </label>
-        <input name="taskTitle" id="newTask" type="text" />
+        <input className="rounded" name="taskTitle" id="newTask" type="text" />
 
         <button type="submit">Add</button>
       </form>
 
-      <List list={taskList} />
+      <List list={taskList} onRemoveTask={handleRemoveTask} />
     </>
   );
 }
 
-const List = ({ list }) => (
+const List = ({ list, onRemoveTask }) => (
   <ul>
     {list?.map((item) => (
       <li key={item.objectID}>
         <div>
           {item.title} {item.currentStatus}
-          <button>remove</button>
+          <button onClick={() => onRemoveTask(item)}>Remove</button>
         </div>
       </li>
     ))}
