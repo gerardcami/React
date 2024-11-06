f5d6c0432013727e704df56ba2af7c3bb5eed0f4
Gerard Camí <gerardcamigay@gmail.com>
2024-11-06 19:49:16 +0100

    Finished

---
+++ projects/06-prueba-tecnica-clima/README.md
@@ -0,0 +1,21 @@
+<!-- Objetivo
+Crear una aplicación web que permita al usuario ingresar una
+ubicación y obtener el clima actual de esa área utlizando la API
+de OpenweatherMap. La prueba se realizará con React.
+
+Requisitos
+Interfaz de Usuario:
+Un campo de entrada para que el usuario ingrese el nombre de la ciudad.
+Un botón para enviar la consulta.
+Un área para mostrarlos resultados del clima (temperatura,
+condiciones meteorológicas, humedad, etc ... ).
+Manejo de errores para situaciones como ciudad no encontrada.
+
+API de clima:
+Utilizar la API de OpenWeatherMap (https://openweathermap.org/api) para
+obtener los datos meteorológicos.
+Necesitarás registrarte y obtener una clave de API.
+
+Funciones:
+Hacer una llamada a la API al enviar el formulario.
+Procesar la respuesta y mostrar los datos en la interfaz. -->
---
+++ projects/06-prueba-tecnica-clima/src/App.jsx
@@ -0,0 +1,133 @@
+import { useCallback, useEffect, useReducer, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+const API_KEY = "651f532994cdb08d22c53ecea00b531b";
+
+// Custom hook para almacenar el término que estamos buscando en el
+// localStorage y recuperarlo cada vez que usamos la aplicación.
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+function App() {
+  const weatherDataReducer = (state, action) => {
+    switch (action.type) {
+      case "DATA_FETCH_INIT":
+        return {
+          ...state,
+          isLoading: true,
+          isError: false,
+        };
+      case "DATA_FETCH_SUCCESS":
+        return {
+          ...state,
+          isLoading: false,
+          isError: false,
+          data: action.payload,
+        };
+      case "DATA_FETCH_FAILURE":
+        return {
+          ...state,
+          isLoading: false,
+          isError: true,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [searchTerm, setSearchTerm] = useStorageState("search", "Barcelona");
+
+  // gestionar todas las variables de estado que
+  // estén relacionadas entre si.
+  const [url, setUrl] = useState("");
+  const [weatherData, dispatchWeatherData] = useReducer(weatherDataReducer, {
+    data: null,
+    isloading: false,
+    isError: false,
+  });
+
+  const [geoUrl, setGeoUrl] = useState(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=1&appid=${API_KEY}`
+  );
+
+  const handleSearchInput = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    fetchWeatherData();
+    event.preventDefault();
+  };
+
+  const fetchWeatherData = async () => {
+    dispatchWeatherData({ type: "DATA_FETCH_INIT" });
+    try {
+      // Obtener geolocalización de la ciudad
+      const geoResponse = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=1&appid=${API_KEY}`
+      );
+      const { lat, lon } = geoResponse.data[0];
+
+      // Hacer la llamada a la API de clima usando latitud y longitud
+      const weatherResponse = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+      );
+
+      dispatchWeatherData({
+        type: "DATA_FETCH_SUCCESS",
+        payload: weatherResponse.data.main,
+      });
+    } catch {
+      dispatchWeatherData({ type: "DATA_FETCH_FAILURE" });
+    }
+  };
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
+
+  return (
+    <>
+      <h1>Prueba técnica: API tiempo</h1>
+      <hr />
+      <form onSubmit={handleSearchSubmit}>
+        <label htmlFor="search">Consulta el clima por ciudad: </label>
+        <input
+          id="search"
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchInput}
+        />
+        <button type="submit" disabled={!searchTerm}>
+          Consultar
+        </button>
+      </form>
+
+      <div>
+        <h2>Resultados:</h2>
+        {weatherData.isError ? (
+          <p>Error al obtener los datos.</p>
+        ) : weatherData.isLoading ? (
+          <p>Cargando...</p>
+        ) : (
+          weatherData.data && (
+            <>
+              <p>Temperatura: {weatherData.data.temp} °C</p>
+              <p>Humedad: {weatherData.data.humidity} %</p>
+            </>
+          )
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
