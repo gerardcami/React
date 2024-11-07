fe9e3c279398b04d6deeaef327091d44fdc7aea1
Gerard Camí <gerardcamigay@gmail.com>
2024-11-07 22:59:11 +0100

    Products are being added to the card correctly

---
+++ projects/07-online-shop/src/App.jsx
@@ -0,0 +1,118 @@
+import data from "./data/data.json";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const addItemId = (list) => {
+  const newList = list.map((item) => ({
+    ...item,
+    objectID: crypto.randomUUID(),
+  }));
+
+  return newList;
+};
+
+const products = addItemId(data.products);
+
+function App() {
+  const [quantities, setQuantities] = useState([]);
+  const [cartProducts, setCartProducts] = useState(() => {
+    return JSON.parse(localStorage.getItem("cart")) || [];
+  });
+  const [isNotAvailable, setIsNotAvailable] = useState(false);
+
+  const handleQuantityChange = (itemID, newQuantity) => {
+    const newList = quantities.find((item) => item.objectID === itemID)
+      ? quantities.map((item) =>
+          item.objectID === itemID
+            ? { objectID: itemID, amount: newQuantity }
+            : item
+        )
+      : [
+          ...quantities,
+          {
+            objectID: itemID,
+            amount: newQuantity,
+          },
+        ];
+
+    setQuantities(newList);
+  };
+
+  const handleAddCart = (product) => {
+    const amount = quantities.find(
+      (item) => item.objectID === product.objectID
+    )?.amount;
+
+    const newCartList = cartProducts.find(
+      (item) => item.objectID === product.objectID
+    )
+      ? cartProducts.map((item) =>
+          item.objectID === product.objectID
+            ? { objectID: product.objectID, amount: amount }
+            : item
+        )
+      : [
+          ...cartProducts,
+          {
+            objectID: product.objectID,
+            amount: amount,
+          },
+        ];
+
+    setCartProducts(newCartList);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartProducts));
+  }, [cartProducts]);
+
+  return (
+    <>
+      <h1>Alternova Shop</h1>
+      <div>
+        <section>
+          {products.map((product, index) => (
+            <article className="productCard" key={index}>
+              <img
+                className="productCover"
+                src={product.image}
+                alt={`Image: ${product.name}`}
+              />
+              <div>
+                <h2>{product.name}</h2>
+                <form onSubmit={() => handleAddCart(product)}>
+                  <label htmlFor="quantity">Quantity: </label>
+                  <input
+                    type="number"
+                    value={
+                      quantities.find(
+                        (item) => item.objectID === product.objectID
+                      )?.amount || 1
+                    }
+                    name=""
+                    id="quantity"
+                    min={1}
+                    onChange={(event) =>
+                      handleQuantityChange(product.objectID, event.target.value)
+                    }
+                  />
+                  <button type="submit">Add to cart</button>
+                </form>
+              </div>
+            </article>
+          ))}
+        </section>
+        <aside>
+          <h2>Cart</h2>
+          <div>
+            {cartProducts.map((item, index) => (
+              <div key={index}>{item.objectID}</div>
+            ))}
+          </div>
+        </aside>
+      </div>
+    </>
+  );
+}
+
+export default App;
